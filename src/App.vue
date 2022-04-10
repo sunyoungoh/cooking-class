22eeadf8762d2f1c2e7acd56bc7a9bce8a622134
syeong <tjsdud7379@gmail.com>
2022-04-10 13:26:56 +0900

    Add Vuetify, Font, Unicons

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app class="app">
+    <AppHeader />
+    <v-main class="app-contents">
+      <router-view />
+    </v-main>
+    <AppFooter />
+  </v-app>
 </template>
 
+<script>
+import AppHeader from '@/components/common/AppHeader.vue';
+import AppFooter from '@/components/common/AppFooter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import './styles/reset.css';
+@import './styles/common.css';
 </style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,15 @@ import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#141414',
+        anchor: '#141414',
+      },
+    },
+  },
+});
+
+export default vuetify;
