304067a04794ba732bbdcebc0f519e5403b3191f
syeong <tjsdud7379@gmail.com>
2022-04-10 13:17:15 +0900

    Add AppHeader, AppFooter

---
+++ src/components/common/AppFooter.vue
@@ -0,0 +1,60 @@
+<template>
+  <footer>
+    <div class="footer__container">
+      <div class="footer__left">
+        <ul class="footer__nav">
+          <li><a>쿠킹클래스 소개</a></li>
+          <li><a>이용안내</a></li>
+          <li><a>이용약관</a></li>
+          <li>
+            <a><strong>개인정보처리방침</strong></a>
+          </li>
+          <li><a>고객센터</a></li>
+        </ul>
+        <div class="footer__info mt-6">
+          <p>
+            (주)쿠킹클래스 | 대표 홍길동 | 주소 서울특별시 중구 ㅇㅇㅇ로 123 |
+            대표전화 02-0000-0000
+          </p>
+          <p>
+            사업자등록번호 000-00-00000 ㅣ 통신판매번호 제 0000-서울중구-0000
+          </p>
+          <p class="copyright mt-2">
+            ⓒ 2021. Cooking Class All Rights Reserved.
+          </p>
+        </div>
+      </div>
+      <div class="footer__right">
+        <div class="footer__socials">
+          <a
+            href="https://www.facebook.com"
+            target="”_blank”"
+            class="footer__social"
+          >
+            <img src="@/assets/img/social/facebook.png" />
+          </a>
+          <a
+            href="https://www.instagram.com/"
+            target="”_blank”"
+            class="footer__social"
+          >
+            <img src="@/assets/img/social/insta.png" />
+          </a>
+          <a
+            href="https://www.youtube.com"
+            target="”_blank”"
+            class="footer__social"
+          >
+            <img src="@/assets/img/social/youtube.png" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/common/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <router-link to="/">
+      <img src="@/assets/img/logo.svg" alt="logo" class="logo" />
+    </router-link>
+    <nav class="nav__list">
+      <router-link to="/korean">한식</router-link>
+      <router-link to="/western">양식</router-link>
+      <router-link to="/japanese">일식</router-link>
+      <router-link to="/chinese">중식</router-link>
+      <router-link to="/baking">제과·제빵</router-link>
+      <router-link to="/baking">바리스타</router-link>
+      <router-link to="/event">이벤트</router-link>
+      <router-link to="/customer">고객센터</router-link>
+    </nav>
+    <div class="nav__icons">
+      <router-link to="/cart"><i class="uil uil-shopping-bag"></i></router-link>
+      <router-link to="/login"><i class="uil uil-user"></i></router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
